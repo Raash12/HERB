83c1f8306bd80b762dc130ce48ce7a5a9f304a53
abdirashiid <abdirashiidbile5@gmail.com>
2026-04-18 17:05:01 +0300

    optimized build and code splitting

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,26 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ 
+      presets: [reactCompilerPreset()] 
+    })
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600, // Waxay kordhinaysaa xadka ilaa 1.6MB si looga fogaado digniinta
+    rollupOptions: {
+      output: {
+        // Tani waxay u kala jebinaysaa node_modules faylalyo yaryar (Code Splitting)
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
     },
   },
 })
