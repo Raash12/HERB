0957599da04231130d8e9b37e26f2ac5bb8d8ea3
Zakai Ikar <zakariyeiikar669@gmail.com>
2026-04-23 16:03:25 +0300

    added session expire

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Login from "./pages/index";
 import AdminDashboard from "./pages/dashboard/admin";
 import DoctorDashboard from "./pages/dashboard/doctor";
 import ReceptionDashboard from "./pages/dashboard/reception";
 import DoctorAppointments from "./pages/doctor/DoctorAppointments";
 import MedicalPrescription from "./pages/doctor/MedicalPrescription"; 
-import OpticalPrescriptionPage from "./pages/doctor/OpticalPrescriptionPage"; // Magaca saxda ah waa kan
+import OpticalPrescriptionPage from "./pages/doctor/OpticalPrescriptionPage"; 
 import DoctorPrescriptionView from "./pages/doctor/DoctorPrescriptionView";
 
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      
-      {/* Admin Dashboard */}
-      <Route path="/dashboard/admin" element={<AdminDashboard />} />
-      
-      {/* Doctor & Reception Dashboards */}
-      <Route path="/dashboard/doctor" element={<DoctorDashboard />} />
-      <Route path="/dashboard/reception" element={<ReceptionDashboard />} />
-      
-      {/* Doctor Routes */}
-      <Route path="/dashboard/doctor/appointments" element={<DoctorAppointments />} />
-      
-      {/* Halkan hoose ayaan ka beddelay PrescriptionPage una beddelay OpticalPrescriptionPage 
-          si uu ula mid noqdo import-ka sare 
-      */}
-      <Route path="/doctor/prescription/:id" element={<OpticalPrescriptionPage />} />
-      <Route path="/doctor/medical-prescription/:id" element={<MedicalPrescription />} />
-      <Route path="/doctor/optical-prescription/:id" element={<OpticalPrescriptionPage />} />
-      <Route path="/doctor/prescriptions" element={<DoctorPrescriptionView />} />
+// Import your SessionTimeout component
+import SessionTimeout from "./SessionTimeout";
 
-    </Routes>
+export default function App() {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* The timer will only activate if the user is NOT on the login page ("/")
+          This prevents the app from trying to log out a user who hasn't logged in yet.
+      */}
+      {location.pathname !== "/" && <SessionTimeout />}
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        
+        {/* Admin Dashboard */}
+        <Route path="/dashboard/admin" element={<AdminDashboard />} />
+        
+        {/* Doctor & Reception Dashboards */}
+        <Route path="/dashboard/doctor" element={<DoctorDashboard />} />
+        <Route path="/dashboard/reception" element={<ReceptionDashboard />} />
+        
+        {/* Doctor Routes */}
+        <Route path="/dashboard/doctor/appointments" element={<DoctorAppointments />} />
+        
+        {/* Prescription Routes */}
+        <Route path="/doctor/prescription/:id" element={<OpticalPrescriptionPage />} />
+        <Route path="/doctor/medical-prescription/:id" element={<MedicalPrescription />} />
+        <Route path="/doctor/optical-prescription/:id" element={<OpticalPrescriptionPage />} />
+        <Route path="/doctor/prescriptions" element={<DoctorPrescriptionView />} />
+      </Routes>
+    </>
   );
 }
